a5e748f356ef64c77d103e4dbc0ededf86795032
Nam Truong <thnam1410@gmail.com>
2021-04-04 16:21:32 +0700

    update function update role user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,10 +25,21 @@ exports.getAllUser = async (req, res) => {
             page: parseInt(page, 10) || 1,
             limit: parseInt(limit, 10) || 20,
         };
-        const users = await User.paginate({}, options);
+        const users = await User.paginate({ role : {$ne: 'Admin'}}, options);
         return res.status(200).json(users);
     } catch (err) {
         console.log(err);
         return res.status(500).json({ message: "Server Error" });
+    }
+}
+
+// [POST] /user/update-role
+exports.updateUserRole = async (req, res) => {
+    try {
+        const { _id, role } = req.body;
+        const user = await User.findByIdAndUpdate(_id, { role }, { new: true })
+        return res.status(200).json(user);
+    }catch (err) {
+        return res.status(500).json({ message: "Server Error"})
     }
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,4 +1,4 @@
-const {getUserTransactionDetails, getAllUser} = require("../controllers/user.controller");
+const {getUserTransactionDetails, getAllUser, updateUserRole} = require("../controllers/user.controller");
 const {authUser, authAdmin} = require("../middlewares/auth");
 const express = require('express')
 const router = express.Router()
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.get("/transaction", authUser, getUserTransactionDetails);
 router.get("/index",authAdmin , getAllUser)
+router.post('/update-role', authAdmin, updateUserRole)
 
 module.exports = router;
